eea021c42d9d40fdcce237244f20da33cae74d7f
Michael Meuli <michael.meuli@gmail.com>
2022-10-08 16:29:14 +0200

    first commit

---
+++ tbprofiler_custom_script.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import sys
+import csv
+import json
+import argparse
+import os
+from collections import defaultdict
+from tqdm import tqdm
+import pathogenprofiler as pp
+import tbprofiler as tbprofiler
+
+def main(args):
+    bed_file = "%s/share/tbprofiler/%s.bed" % (sys.base_prefix,args.db)
+    locus_tag2drugs = tbprofiler.get_lt2drugs(bed_file)
+
+    if args.samples:
+        samples = [x.rstrip() for x in open(args.samples).readlines()]
+    else:
+        samples = [x.replace(args.suffix,"") for x in os.listdir(args.dir) if x[-len(args.suffix):]==args.suffix]
+
+    for s in tqdm(samples):
+        data = json.load(open(pp.filecheck("%s/%s%s" % (args.dir,s,args.suffix))))
+
+
+parser = argparse.ArgumentParser(description='tbprofiler script',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--samples',type=str,help='File with samples')
+parser.add_argument('--dir',default="results/",type=str,help='Directory containing results')
+parser.add_argument('--db',default="tbdb",type=str,help='Database name')
+parser.add_argument('--suffix',default=".results.json",type=str,help='File suffix')
+parser.set_defaults(func=main)
+
+args = parser.parse_args()
+args.func(args)
